e5ea66f1fe72d2558bfebf76a7f19b29a50953a3
Solomon <solomonlazio@gmail.com>
2018-12-20 07:20:39 +0000

    update Today's log:)

---
+++ 04_Strings_and_Interactions/tabbify_my_csv.py
@@ -0,0 +1,23 @@
+"""
+small copy program that turns a csv file into a tabbed file
+
+"""
+import os
+
+def tabbify(infilename,outfilename,ignore_comments=True,comment_chars="#;/"):
+    """
+INPUT: infilename
+OUTPUT: creates a file called outfilename
+    """
+    if not os.path.exists(infilename):
+        return  # do nothing if the file isn't there
+    f = open(infilename,"r")
+    o = open(outfilename,"w")
+    inlines = f.readlines() ; f.close()
+    outlines = []
+    for l in inlines:
+        if ignore_comments and (l[0] in comment_chars):
+            outlines.append(l)
+        else:
+            outlines.append(l.replace(",","\t"))
+    o.writelines(outlines) ; o.close()
