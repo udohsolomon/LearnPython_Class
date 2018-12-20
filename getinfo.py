b0b0d8d15d0c3832757f3f4e4823efb9974f4a04
Solomon <solomonlazio@gmail.com>
2018-12-20 01:33:08 +0000

    update Today's log:)

---
+++ getinfo.py
@@ -0,0 +1,16 @@
+
+import os
+import sys
+
+def getinfo(path="."):
+    """
+Purpose: make simple use of os and sys modules
+Input: path (default = "."), the directory you want to list
+    """
+    print("You are using Python version ", end= " ")
+    print(sys.version)
+    print(" ")
+    print("-" * 90)
+    print("Files in the directory " + str(os.path.abspath(path)) + ":")
+    for f in os.listdir(path): 
+        print(f)
---
+++ script_name.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+'''
+Docstring for ths module
+
+'''
+# All your module stuffs here ...
+
+#At the bottom stick ....
+
+if __name__ == "__main__":
+    
+    '''
+    Only executed if this module is called from the command line
+    
+    '''
+    
+    # Can call functions from within this module
+    print('I was called from the command line!')
