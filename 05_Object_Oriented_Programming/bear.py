3c77b3eb0c6a81120f346284da26479a1851ce18
Solomon <solomonlazio@gmail.com>
2018-12-23 04:52:50 +0000

    update Today's log:)

---
+++ 05_Object_Oriented_Programming/bear.py
@@ -0,0 +1,30 @@
+import datetime
+class Bear:
+    logfile_name = 'bear_log'
+    bear_num     = 0
+    bear_names   = []
+    def __init__(self, name):
+        self.name = name
+        print('Made a bear called %s' %(name))
+        self.logfile = open(bear.logfile_name, 'a')
+        Bear.bear_num += 1
+        self.my_num = Bear.bear_num
+        self.logfile.write('[%s] created bear #%i named %s\n' %
+                          (datetime.datetime.now(), Bear.bear_num, self.name))
+        self.logfile.flush()
+        
+    def growl(self, nbeep = 5):
+        print('\a'*nbeep)
+        
+    def __del__(self):
+        print('Bang! %s is no longer' % self.name)
+        self.logfile.write('[%s] deleted bear #%i named %s\n' % 
+                          datetime.datetime.now(), self.my_num, self.name)
+        self.logfile.flush()
+        #Decrease the number of bears in the population
+        Bear.bear_num -= 1
+        self.logfile.close()
+        
+    def __str__(self):
+        return 'Name = %s bear number = %i (population %i)' %
+                (self.name, self.my_num, Bear.bear_num)
