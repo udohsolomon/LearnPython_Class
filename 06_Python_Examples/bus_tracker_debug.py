f361d05121e9418e772c722a2f8d31f8ff972472
Solomon <solomonlazio@gmail.com>
2018-12-23 21:15:13 +0000

    update Today's log:)

--- 06_Python_Examples/bus_tracker_debug.py
+++ 06_Python_Examples/bus_tracker_debug.py
@@ -14,11 +14,19 @@ stopid = sys.argv[2]
 import urllib.request
 
 
-u  = urllib.request.urlopen('http://ctabustracker.com/bustime/map/getStopPredictions.jsp?route={}&stop={}'.format(route, stopid))
+u  = urllib.request.urlopen('http://ctabustracker.com/bustime/map/getStopPrediction.jsp?route={}&stop={}'.format(route, stopid))
+
+#import pdb; pdb.set_trace() #Launch debugger
 data = u.read()
+
+#print('Response:', data) # Use print statement for debugging
 
 from xml.etree.ElementTree import XML
 doc = XML(data)
 
 for pt in doc.findall('.//pt'):
     print(pt.text)
+
+#python3 -i bus_tracker_debug.py
+#import pdb 
+#pdb.pm()
