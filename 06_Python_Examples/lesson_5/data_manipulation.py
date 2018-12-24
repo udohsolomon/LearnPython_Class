36f0d37fee3fa921e0e83940e6fc19c2b0317e53
Solomon <solomonlazio@gmail.com>
2018-12-24 09:00:12 +0000

    update Today's log:)

--- 06_Python_Examples/lesson_5/data_manipulation.py
+++ 06_Python_Examples/lesson_5/data_manipulation.py
@@ -23,3 +23,22 @@ more_100
 # List comprehension
 
 total = sum([holding['shares'] * holding['price'] for holding in portfolio])
+names = [holding['name'] for holding in portfolio]
+more100 = [holding for holding in portfolio if holding['shares'] > 100]
+
+# getting the current stock prices of these stocks
+# unique_names = set(names)
+# namestr = ','.join(unique_names)
+# import urllib.request
+# u = urllib.request.urlopen('http://finance.yahoo.com/d/quotes.csv?s={}&f=l1'.format(namestr))
+# data = u.read()
+
+# Sorting and Grouping
+
+def holding_name(holding):
+    return holding['name']
+
+holding_name(portfolio[0])
+portfolio.sort(key = holding_name)
+for holding in portfolio:
+    print(holding)
