78e1cd614da2d404006200792ee840e39f15744b
Solomon <solomonlazio@gmail.com>
2018-12-26 05:40:05 +0000

    update Today's log:)

---
+++ 06_Python_Examples/lesson_7/holding.py
@@ -0,0 +1,7 @@
+
+class Holding(object):
+    def __init__(self, name, date, shares, price):
+        self.name = name
+        self.date = date
+        self.shares = shares
+        self.price = price
