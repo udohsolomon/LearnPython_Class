330605490a4078303fd71346b888b350ad392b79
Solomon <solomonlazio@gmail.com>
2018-12-26 05:50:50 +0000

    update Today's log:)

--- 06_Python_Examples/lesson_7/holding.py
+++ 06_Python_Examples/lesson_7/holding.py
@@ -1,7 +1,15 @@
 
-class Holding(object):
+class Holding(object): 
+    '''
+    A collection of functions and the functions actually serve as methods
+    one access through the . operation.
+    '''
+    
     def __init__(self, name, date, shares, price):
         self.name = name
         self.date = date
         self.shares = shares
         self.price = price
+
+    def cost(self): #define a cost method that carries out calculation
+        return self.shares * self.price
