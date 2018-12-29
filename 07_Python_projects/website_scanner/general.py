fc0284844def528d630f650a332a9e7e055ca4c4
Solomon <solomonlazio@gmail.com>
2018-12-29 13:53:29 +0000

    update Today's log:)

---
+++ 07_Python_projects/website_scanner/general.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env/python3
+
+import os
+
+def create_dir(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+def write_file(path, data):
+    with open(path, 'w') as f:
+        f.write(data)
+        f.close()
