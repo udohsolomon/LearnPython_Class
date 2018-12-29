00fec2f416f0f9df1e97126fcbe05b7d6d3df4bf
Solomon <solomonlazio@gmail.com>
2018-12-29 23:38:42 +0000

    update Today's log:)

---
+++ 07_Python_projects/website_scanner/robots.py
@@ -0,0 +1,15 @@
+import urllib.request
+import os
+
+def get_robots(url):
+    if url.endswith('/'):
+        path = url
+    else:
+        path = url + '/'
+
+    req = urllib.request.urlopen(path + 'robots.txt', data=None)
+    data = io.TextIOwrapper(req, encoding = 'utf-8')
+    return data.read()
+
+print(get_robots('https://www.coventry.ac.uk/'))
+
