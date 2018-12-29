e8c2881b26568e49523461eca187d7c9345ae2b6
Solomon <solomonlazio@gmail.com>
2018-12-29 14:00:01 +0000

    update Today's log:)

---
+++ 07_Python_projects/website_scanner/domain_name.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env/python3
+
+from tld import get_tld
+
+def get_domain_name(url):
+    domain_name = get_tld(url)
+    return domain_name
+
+print(get_domain_name('http://augeosai.com'))
