5c96d7c0064539373dfbe846225035e41957a5cd
Solomon <solomonlazio@gmail.com>
2018-12-29 23:44:51 +0000

    update Today's log:)

--- 07_Python_projects/website_scanner/robots.py
+++ 07_Python_projects/website_scanner/robots.py
@@ -8,7 +8,7 @@ def get_robots(url):
         path = url + '/'
 
     req = urllib.request.urlopen(path + 'robots.txt', data=None)
-    data = io.TextIOwrapper(req, encoding = 'utf-8')
+    data = io.TextIOWrapper(req, encoding = 'utf-8')
     return data.read()
 
 print(get_robots('https://www.coventry.ac.uk/'))
