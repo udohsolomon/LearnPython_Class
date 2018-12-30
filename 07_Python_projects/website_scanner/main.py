1134516becaf9c2445a79fcf8c934a8f31090cae
Solomon <solomonlazio@gmail.com>
2018-12-30 00:35:40 +0000

    update Today's log:)

--- 07_Python_projects/website_scanner/main.py
+++ 07_Python_projects/website_scanner/main.py
@@ -19,10 +19,10 @@ def gather_info(name, url):
 def create_report(name, full_url, domain_name, nmap, robots, whois):
     project_dir = ROOT_DIR + '/' + name
     create_dir(project_dir)
-    write_file(project_dir + 'full_url', full_url)
-    write_file(project_dir + 'domain_name', domain_name)
-    write_file(project_dir + 'nmap', + nmap)
-    write_file(project_dir + 'robots_txt', robots)
-    write_file(project_dir + 'whois', + whois)
+    write_file(project_dir + '/full_url', full_url)
+    write_file(project_dir + '/domain_name', domain_name)
+    write_file(project_dir + '/nmap', + nmap)
+    write_file(project_dir + '/robots_txt', robots)
+    write_file(project_dir + '/whois', + whois)
 
 gather_info('Coventry Uni', 'https://www.coventry.ac.uk/')
