fa8b52a69802bd0b243f6553631b2271d4361c90
Solomon <solomonlazio@gmail.com>
2018-12-30 00:19:24 +0000

    update Today's log:)

---
+++ 07_Python_projects/website_scanner/main.py
@@ -0,0 +1,9 @@
+from general import *
+from domain_name import *
+from ip_address import *
+from nmap import *
+from robots import *
+from whois import *  #These are bad coding practice, don't try these in actual production environments
+
+ROOT_DIR = 'companies'
+create_dir(ROOT_DIR)
