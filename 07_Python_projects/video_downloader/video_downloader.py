ae394a891337610d4420584ed4e43edb2940f814
Solomon <solomonlazio@gmail.com>
2019-01-03 03:08:55 +0000

    update Today's log:)

--- 07_Python_projects/video_downloader/video_downloader.py
+++ 07_Python_projects/video_downloader/video_downloader.py
@@ -10,3 +10,15 @@ all the links and then download videos.
 '''
 # specify the url of the link here
 archive_url = "http://www-personal.umich.edu/~csev/books/py4inf/media/"
+
+def get_links():
+
+    # create response object
+    r = requests.get(archive_url)
+    # create Beautiful soup object
+    soup bs(r.content, 'html5lib')
+    # find all links on the web page
+    links = soup.findAll('a')
+    # filter the links sending with .mp4
+    video_links = [archive_url + links['href'] for link in links if link['href'].endswith('mp4')]
+    return video_links
